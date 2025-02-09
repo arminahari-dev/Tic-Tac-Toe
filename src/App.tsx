8eb94bfde30ac3156fe2e0ba11e66e97e42fad99
armin ahari <ahariarmin@gmail.com>
2025-02-09 11:37:15 +0330

    feat: Add game header, 3x3 matrix, and integrate react-hot-toast with dark color scheme
    Implement a 3x3 tic-tac-toe matrix with controlled inputs for 'x' and 'o'.
    Restrict inputs based on the current player's turn.
    Disable cells after a valid move to prevent overwriting.
    Provide visual indicators for the current player.
    Display error toast for invalid inputs.
    Change the color scheme to dark mode.
    Add react-hot-toast dependency for notifications.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import MatrixComponent from "./components/MatrixComponent";
+import { Toaster } from "react-hot-toast";
+
 function App() {
-  return <h1 className="font-bold text-3xl underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-col justify-center items-center gap-4 h-screen">
+      <h1 className="text-4xl">Tic Tac Toe game</h1>
+      <MatrixComponent />
+      <Toaster />
+    </div>
+  );
 }
 
 export default App;
